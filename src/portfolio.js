bcd39407b007200f24825b9b2ed47129c801a185
srishti3418 <srishti.suman35@gmail.com>
2021-08-26 19:06:55 +0530

    changes made

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,7 +11,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Srishti Suman",
+  username: "Srishti Sumn",
   title: "Hi all, I'm Srishti",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks."
